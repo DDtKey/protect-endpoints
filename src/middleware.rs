b24445e9fd98934300299314ba6670237bc1b9d9
DDtKey <ddttkey@gmail.com>
2021-01-18 20:25:23 +0300

    docs: fix typo

--- src/middleware.rs
+++ src/middleware.rs
@@ -72,7 +72,7 @@ where
     /// }
     /// ```
     ///
-    ///[`AuthoritiesExtractor`]: actix_web_grants::authorities::AuthoritiesExtractor
+    ///[`AuthoritiesExtractor`]: crate::authorities::AuthoritiesExtractor
     pub fn with_extractor(extractor: T) -> GrantsMiddleware<T> {
         GrantsMiddleware {
             extractor: Arc::new(extractor),
