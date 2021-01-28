5a5c4a226653f1dbce79fe40ec1035f0b08926bb
DDtKey <ddttkey@gmail.com>
2021-01-28 19:12:25 +0300

    docs(examples): upd old description

--- examples/jwt-httpauth/src/main.rs
+++ examples/jwt-httpauth/src/main.rs
@@ -15,13 +15,13 @@ mod claims;
 
 #[get("/admin")]
 #[has_permissions("OP_GET_SECURED_INFO")]
-// For the `ADMIN` - endpoint will give the HTTP status 200, otherwise - 403
+// For the user with permission `OP_GET_SECURED_INFO` - endpoint will give the HTTP status 200, otherwise - 403
 // You can check via cURL (for generate you own token, use `/token` handler):
 // ```sh
 //  curl --location --request GET 'http://localhost:8080/api/admin' \
 //  --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkxvcmVtLUlwc3VtIiwicGVybWlzc2lvbnMiOlsiT1BfR0VUX1NFQ1VSRURfSU5GTyJdLCJleHAiOjE5MjY2ODk3MTF9.vFZ6qYRhJ4KY3trXvIAnhTed8UXxCw2tCSB4Qz5D7So'
 // ```
-async fn admin_secured() -> HttpResponse {
+async fn permission_secured() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -51,7 +51,7 @@ async fn main() -> std::io::Result<()> {
         App::new().service(create_token).service(
             web::scope("/api")
                 .wrap(auth)
-                .service(admin_secured)
+                .service(permission_secured)
                 .service(manager_secured),
         )
     })
